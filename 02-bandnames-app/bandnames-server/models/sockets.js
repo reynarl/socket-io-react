87a8e779c85d1a014aa0d6e3f06f49382ec9c09c
Reeyna <reeynarl@gmail.com>
2024-06-13 15:58:31 -0600

    Add event for increase votes

--- 02-bandnames-app/bandnames-server/models/sockets.js
+++ 02-bandnames-app/bandnames-server/models/sockets.js
@@ -20,6 +20,13 @@ class Sockets {
 
             socket.emit('current-bands', this.bandList.getBands())
 
+            //escuchando el evento para agregar votos pasando el id y llamando la funcion increaseVotes() creada previamente para incrementar.
+            socket.on('add-vote', (id) => {
+                this.bandList.increaseVotes(id)
+                //volvemos a emitir toda la lista de bandas para refrescar, y se visualizen los cambios
+                //usamos this.io para que los cambios se generen en todas las paginas abiertas
+                this.io.emit('current-bands', this.bandList.getBands())
+            })
 
         });
     }
